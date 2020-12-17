48a9c9fbe68295ebcf837e73a7d3902f90cde8ba
Jay <mantine08@gmail.com>
2020-12-16 23:33:33 -0800

    Added parallaxing

--- assets/js/intro-page.js
+++ assets/js/intro-page.js
@@ -5,23 +5,25 @@ $(document).ready(function () {
   window.addEventListener("wheel", function (event) {
     if (!inAnimation) {
       if (event.deltaY < 0) {
+        // scroll up
         scroll--;
         if (scroll <= 0) {
           inAnimation = true;
           scroll = 0;
           $(".intro-div").show();
+          $(".scroll-div").show();
           $(".grow-container").hide();
           $(".grow-container").css("opacity", "0");
           $(".select-background-div").animate(
             {
-              backgroundColor: " hsl(0, 8%, 85%)",
+              backgroundColor: "white",
             },
             500,
             function () {
               // Animation complete.
               $(".select-background-div")
                 .removeClass("select-background-div")
-                .addClass("intro-background-div");
+                .addClass("intro-background-img");
             }
           );
           $(".app-bar").animate(
@@ -36,12 +38,14 @@ $(document).ready(function () {
           );
         }
       } else if (event.deltaY > 0) {
+        // scroll down
         scroll++;
-        if (scroll >= 4) {
+        if (scroll >= 1) {
           inAnimation = true;
-          scroll = 4;
-          $(".intro-background-div")
-            .removeClass("intro-background-div")
+          scroll = 1;
+
+          $(".intro-background-img")
+            .removeClass("intro-background-img")
             .addClass("select-background-div");
           $(".select-background-div").animate(
             {
@@ -50,6 +54,7 @@ $(document).ready(function () {
             500,
             function () {
               // Animation complete.
+              $(".scroll-div").hide();
               $(".intro-div").hide();
               $(".grow-container").show();
               $(".grow-container").animate(
@@ -75,24 +80,22 @@ $(document).ready(function () {
           );
         }
       }
-      $(".intro-div").animate(
-        {
-          opacity: 1 - (scroll % 5) / 4,
-        },
-        200,
-        function () {
-          // Animation complete.
-        }
-      );
-      $(".scroll-div").animate(
-        {
-          opacity: 1 - (scroll % 5) / 4,
-        },
-        200,
-        function () {
-          // Animation complete.
-        }
-      );
     }
+  });
+  $("html").mousemove(function (e) {
+    var wx = $(window).width();
+    var wy = $(window).height();
+
+    var x = e.pageX - this.offsetLeft;
+    var y = e.pageY - this.offsetTop;
+
+    var newx = x - wx / 2;
+    var newy = y - wy / 2;
+
+    $("#wrapper div").each(function () {
+      var speed = $(this).attr("data-speed");
+      if ($(this).attr("data-revert")) speed *= -1;
+      TweenMax.to($(this), 1, { x: 1 - newx * speed, y: 1 - newy * speed });
+    });
   });
 });
